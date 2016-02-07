fea212e8e222997baea1720da5a3d568333fb45f
mschaffrath <mscha008@ucr.edu>
2016-02-06 20:31:14 -0800

    Create echo.h
    added echo header file

---
+++ echo.h
@@ -0,0 +1,15 @@
+#ifndef echo_H
+#define echo_H
+
+using namespace std;
+
+class echo : public commandComponent
+{
+    private:
+    
+    public:
+        int execute( vector<string>& );
+        
+};
+
+#endif
